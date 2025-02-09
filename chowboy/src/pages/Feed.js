94f3a9f1a64e7e9b026ec86f287fb48c8b78fd9b
mansimadhani <madhanimansi56@gmail.com>
2025-02-09 08:32:55 -0600

    all pages complete

--- chowboy/src/pages/Feed.js
+++ chowboy/src/pages/Feed.js
@@ -8,19 +8,22 @@ import '../styles/Feed.css'
 function Feed() {
   return (
     <div id="feed">
-    <div class="main-left">
+    <div class="main-left-feed">
         <MainBar/>
         <div id="cards">
         <FeedCard title="CABO BOBS IS GIVING OUT FREE BURRITOS FOR THE NEXT 10 MINUTES ONLY! HURRY! They're still giving away some and I just got mine." upvotes={100}></FeedCard>
-        <FeedCard title="Some org is handing out ice cream by Littlefield Foundation!! They only have a little more left, so if you're close come quick!!" upvotes={25}></FeedCard>
-        <FeedCard title="West Campus Collective starting soon! Line is getting long :(" upvotes={3}></FeedCard>
+        <FeedCard title="Some org is handing out ice cream by Littlefield Foundation!! They only have a little more left, so if you're close come quick!! They have strawberry cheesecake too." upvotes={25}></FeedCard>
+        <FeedCard title="West Campus Collective starting soon! They're across the street from where they usually are. They have vegan options today! Come quick bc line is getting long :(" upvotes={3}></FeedCard>
+        
         </div>
-        <img class="send-img" src="/images/send.png" height="auto" width="35px"></img>
+        <div class="send-msg">
+          <input id="input-text" type="text"/>
+          <img class="send-img" src="/images/send.png" height="40px" width="40px"></img>
+        </div>
+    </div> 
+    <div class="main-right-feed">
+      <img id="map" src="/images/map.png" width="650" height="650"></img>
     </div>
-        <img id="map" src="/images/map.png" width="650" height="550" margin-left="400px"></img>
-        <input id="input-text" type="text"/>
-      <div class="main-right">
-      </div>
     </div>
   );
   
